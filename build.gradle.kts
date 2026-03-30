6fead2a54a466726bd94800601439adc54cc5e9e
da-pinci <ochikobore.me@gmail.com>
2026-03-30 17:28:20 +0900

    [add] integrate Kotlinx Benchmark and add benchmark for loop operations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     kotlin("jvm") version "2.2.20"
+    // https://github.com/kotlin/kotlinx-benchmark
+    id("org.jetbrains.kotlinx.benchmark") version "0.4.16"
+    kotlin("plugin.allopen") version "2.2.20"
 }
 
 group = "da.pinci"
@@ -11,6 +14,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-benchmark-runtime:0.4.16")
     testImplementation(kotlin("test"))
 }
 
@@ -19,4 +23,22 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(23)
+}
+
+allOpen {
+    // open all classes annotated with @State, which is required by JMH
+    annotation("org.openjdk.jmh.annotations.State")
+}
+benchmark {
+    targets {
+        register("main")
+    }
+    configurations {
+        named("main") {
+            warmups = 3
+            iterations = 5
+            iterationTime = 1
+            iterationTimeUnit = "s"
+        }
+    }
 }
